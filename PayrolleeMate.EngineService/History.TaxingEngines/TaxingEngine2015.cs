10467d8e7831d6c84a6b7168ee4ad452cd86a76c
ladislavlisy <ladislav.lisy@seznam.cz>
2015-04-28 21:05:16 +0200

    Dodatek zdroje

---
+++ PayrolleeMate.EngineService/History.TaxingEngines/TaxingEngine2015.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PayrolleeMate.EngineService.Engines.Taxing;
+
+namespace PayrolleeMate.EngineService.History.TaxingEngines
+{
+	public class TaxingEngine2015 : TaxingEnginePrototype
+	{
+		public TaxingEngine2015 ()
+			: base(TaxingGuides.Guides2015())
+		{
+		}
+	}
+}
+
